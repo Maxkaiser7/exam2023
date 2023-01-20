e4a1f6de83c674bc052da668867a60f0afe1a591
Maxkaiser7 <maxkaiser950@gmail.com>
2023-01-20 16:15:15 +0100

    creation projet

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig', [
+            'test'=>'test'
+        ]);
+    }
+}
